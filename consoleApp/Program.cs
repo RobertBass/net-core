a0f985d16726fd0d95ef07e2c172f5a883674c98
robertbass <robertbass@hotmail.com>
2024-12-10 08:14:32 -0500

    INITIAL COMMIT

---
+++ consoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingresa tu nombre: ");
+var nombre = Console.ReadLine();
+Console.WriteLine("Ingresa tu edad: ");
+var edad = int.Parse(Console.ReadLine());
+Console.WriteLine($"Hola, mi nombre es {nombre} y tengo {edad.ToWords()} anios");
+
